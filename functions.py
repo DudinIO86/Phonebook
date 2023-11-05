54575148ab3a5dea7ffd0673969143a7dd2f137c
Igor Dudin <dudinio@mail.ru>
2023-11-05 14:02:34 +0300

    Добавлен функционал копирования записи с другую книгу

--- functions.py
+++ functions.py
@@ -3,14 +3,15 @@ import os
 from input_data import*
 
 def print_data():
-    with open("phonebook.txt","r",encoding="utf-8") as file:
+    with open("Phonebook/phonebook.txt","r",encoding="utf-8") as file:
         phonebook_str=file.read()
+       
     print(phonebook_str)
     print()
     
 def add_contact():
     new_contact_str=input_data()
-    with open("phonebook.txt","a",encoding="utf-8") as file:
+    with open("Phonebook/phonebook.txt","a",encoding="utf-8") as file:
         file.write(new_contact_str)
 
 def search_contact():
@@ -31,7 +32,7 @@ def search_contact():
     search=input("Введите данные для поиска: ").lower()
     print()
     
-    with open("phonebook.txt","r",encoding="utf-8") as file:
+    with open("Phonebook/phonebook.txt","r",encoding="utf-8") as file:
         contacts_list=file.read().rstrip().split("\n\n")
     
     check_cont=False
@@ -46,3 +47,29 @@ def search_contact():
             
     if not check_cont:
         print("Такого контакта нет.")
+        
+def copy_contacts():
+    with open("Phonebook/phonebook.txt","r",encoding="utf-8") as file:
+        phonebook_str=file.read().rstrip().split("\n\n")
+        phonebook_str2=list(enumerate((phonebook_str),1))
+        
+        for i in range(len(phonebook_str)):
+            s1=phonebook_str2[i][0]
+            s2=phonebook_str2[i][1]
+            print(str(s1)+". "+s2)
+        
+        bool_index=False
+        
+        while bool_index!=True:
+            
+            choice=input("Какой номер записи скопировать: ")
+            
+            if int(choice)>0 and int(choice)<=len(phonebook_str):        
+                copy_contact_str=phonebook_str[int(choice)-1]
+                with open("Phonebook/phonebook_choice.txt","a",encoding="utf-8") as file:
+                    file.write(copy_contact_str)
+                    file.write("\n\n")
+                    bool_index=True
+                    print("Запись скопирована")
+            else:
+                print("Такой позиции нет. Повторите ввод")
--- user_interface.py
+++ user_interface.py
@@ -6,16 +6,17 @@ def interface():
     
     command=""
     os.system("cls")
-    while command!="4":
+    while command!="5":
         print("Меню пользователя:\n"
             "1. Вывод данных на экран\n"
             "2. Добавить контакт\n"
             "3. Поиск контакта\n"
-            "4. Выход\n")
+            "4. Скопировать контакты в отдельную книгу\n"
+            "5. Выход\n")
         
         command=input("Выберите пункт меню: ")
         
-        while command not in ("1","2","3","4"):
+        while command not in ("1","2","3","4","5"):
             print("Некорректный ввод, повторите запрос")
             command=input("Выберите пункт меню: ")
             
@@ -27,5 +28,7 @@ def interface():
             case "3":
                 search_contact()
             case "4":
+                copy_contacts()
+            case "5":
                 print("Завершение программы")
                 
